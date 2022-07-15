a333d354899ee728bd5b9d06a06380da5f67722d
Olalekan Ogundele <olalekan.o.ogundele@gmail.com>
2022-07-15 17:34:04 +0100

    Migrated code to null safety

---
+++ df_portfolio/lib/constants/fonts.dart
@@ -0,0 +1,8 @@
+class Fonts {
+  Fonts._();
+
+  static const String product = 'ProductSans';
+  static const String nexaLight = 'NexaLight';
+  static const String nexaBold = 'NexaBold';
+
+}
---
+++ df_portfolio/lib/ui/home.dart
@@ -0,0 +1,521 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:df_portfolio/constants/assets.dart';
+import 'package:df_portfolio/constants/fonts.dart';
+import 'package:df_portfolio/constants/strings.dart';
+import 'package:df_portfolio/constants/text_styles.dart';
+import 'package:df_portfolio/models/education.dart';
+import 'package:df_portfolio/utils/screen/screen_utils.dart';
+import 'package:df_portfolio/widgets/responsive_widget.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: const Color(0xFFF7F8FA),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: ScreenUtil.getInstance().setWidth(108),
+        ), //144
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(60),
+            child: _buildAppBar(context),
+          ),
+          drawer: _buildDrawer(context),
+          body: LayoutBuilder(
+            builder: (context, constraints) {
+              return _buildBody(context, constraints);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  //AppBar Methods:-------------------------------------------------------------
+  Widget _buildAppBar(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      titleSpacing: 0.0,
+      title: _buildTitle(),
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      actions:
+          !ResponsiveWidget.isSmallScreen(context) ? _buildActions() : null,
+    );
+  }
+
+  Widget _buildTitle() {
+    return RichText(
+      text: TextSpan(
+        // Note: Styles for TextSpans must be explicitly defined.
+        // Child text spans will inherit styles from parent
+        style: const TextStyle(
+          fontSize: 14.0,
+          color: Colors.black,
+        ),
+        children: <TextSpan>[
+          TextSpan(
+            text: Strings.portfoli,
+            style: TextStyles.logo,
+          ),
+          TextSpan(
+            text: Strings.o,
+            style: TextStyles.logo.copyWith(
+              color: const Color(0xFF50AFC0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildActions() {
+    return <Widget>[
+      MaterialButton(
+        child: Text(
+          Strings.menuHome,
+          style: TextStyles.menuItem.copyWith(
+            color: const Color(0xFF50AFC0),
+          ),
+        ),
+        onPressed: () {},
+      ),
+      MaterialButton(
+        child: Text(
+          Strings.menuAbout,
+          style: TextStyles.menuItem,
+        ),
+        onPressed: () {},
+      ),
+      MaterialButton(
+        child: Text(
+          Strings.menuContact,
+          style: TextStyles.menuItem,
+        ),
+        onPressed: () {},
+      ),
+    ];
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return ResponsiveWidget.isSmallScreen(context)
+        ? Drawer(
+            child: ListView(
+              padding: const EdgeInsets.all(20),
+              children: _buildActions(),
+            ),
+          )
+        : const SizedBox.shrink();
+  }
+
+  //Screen Methods:-------------------------------------------------------------
+  Widget _buildBody(BuildContext context, BoxConstraints constraints) {
+    return SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minWidth: constraints.maxWidth,
+          minHeight: constraints.maxHeight,
+        ),
+        child: ResponsiveWidget(
+          largeScreen: _buildLargeScreen(context),
+          mediumScreen: _buildMediumScreen(context),
+          smallScreen: _buildSmallScreen(context),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLargeScreen(BuildContext context) {
+    return IntrinsicHeight(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(flex: 1, child: _buildContent(context)),
+                _buildIllustration(),
+              ],
+            ),
+          ),
+          _buildFooter(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMediumScreen(BuildContext context) {
+    return IntrinsicHeight(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(flex: 1, child: _buildContent(context)),
+              ],
+            ),
+          ),
+          _buildFooter(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSmallScreen(BuildContext context) {
+    return IntrinsicHeight(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: _buildContent(context),
+          ),
+          const Divider(),
+          _buildCopyRightText(context),
+          SizedBox(
+              height: ResponsiveWidget.isSmallScreen(context) ? 12.0 : 0.0),
+          _buildSocialIcons(),
+          SizedBox(
+              height: ResponsiveWidget.isSmallScreen(context) ? 12.0 : 0.0),
+        ],
+      ),
+    );
+  }
+
+  // Body Methods:--------------------------------------------------------------
+  Widget _buildIllustration() {
+    return Image.network(
+      Assets.programmer3,
+      height: ScreenUtil.getInstance().setWidth(345), //480.0
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: ResponsiveWidget.isSmallScreen(context) ? 24.0 : 0.0),
+        _buildAboutMe(context),
+        const SizedBox(height: 4.0),
+        _buildHeadline(context),
+        SizedBox(height: ResponsiveWidget.isSmallScreen(context) ? 12.0 : 24.0),
+        _buildSummary(),
+        SizedBox(height: ResponsiveWidget.isSmallScreen(context) ? 24.0 : 48.0),
+        ResponsiveWidget.isSmallScreen(context)
+            ? Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _buildEducation(),
+                  const SizedBox(height: 24.0),
+                  _buildSkills(context),
+                ],
+              )
+            : _buildSkillsAndEducation(context)
+      ],
+    );
+  }
+
+  Widget _buildAboutMe(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        // Note: Styles for TextSpans must be explicitly defined.
+        // Child text spans will inherit styles from parent
+        style: const TextStyle(
+          fontSize: 14.0,
+          color: Colors.black,
+        ),
+        children: <TextSpan>[
+          TextSpan(
+            text: Strings.about,
+            style: TextStyles.heading.copyWith(
+              fontFamily: Fonts.nexaLight,
+              fontSize: ResponsiveWidget.isSmallScreen(context) ? 36 : 45.0,
+            ),
+          ),
+          TextSpan(
+            text: Strings.me,
+            style: TextStyles.heading.copyWith(
+              color: const Color(0xFF50AFC0),
+              fontSize: ResponsiveWidget.isSmallScreen(context) ? 36 : 45.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeadline(BuildContext context) {
+    return Text(
+      ResponsiveWidget.isSmallScreen(context)
+          ? Strings.headline
+          : Strings.headline.replaceFirst(RegExp(r' f'), '\nf'),
+      style: TextStyles.subHeading,
+    );
+  }
+
+  Widget _buildSummary() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 80.0),
+      child: Text(
+        Strings.summary,
+        style: TextStyles.body,
+      ),
+    );
+  }
+
+  Widget _buildSkillsAndEducation(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: _buildEducation(),
+        ),
+        const SizedBox(width: 40.0),
+        Expanded(
+          flex: 1,
+          child: _buildSkills(context),
+        ),
+      ],
+    );
+  }
+
+  // Skills Methods:------------------------------------------------------------
+  final skills = [
+    'Java',
+    'Kotlin',
+    'Dart',
+    'Flutter',
+    'Android',
+    'iOS',
+    'Xamarin',
+    'Reactive Programming',
+    'Jenkins',
+    'Photoshop',
+    'JFrog Atrtifactory',
+    'Code Magic',
+  ];
+
+  Widget _buildSkills(BuildContext context) {
+    final List<Widget> widgets = skills
+        .map((skill) => Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: _buildSkillChip(context, skill),
+            ))
+        .toList();
+
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _buildSkillsContainerHeading(),
+        Wrap(children: widgets),
+//        _buildNavigationArrows(),
+      ],
+    );
+  }
+
+  Widget _buildSkillsContainerHeading() {
+    return Text(
+      Strings.skillsIhave,
+      style: TextStyles.subHeading,
+    );
+  }
+
+  Widget _buildSkillChip(BuildContext context, String label) {
+    return Chip(
+      label: Text(
+        label,
+        style: TextStyles.chip.copyWith(
+          fontSize: ResponsiveWidget.isSmallScreen(context) ? 10.0 : 11.0,
+        ),
+      ),
+    );
+  }
+
+  // Education Methods:---------------------------------------------------------
+  final educationList = [
+    Education(
+      'Apr 2018',
+      'Present',
+      'Embrace-it Pakistan',
+      'Sr. Software Engineer',
+    ),
+    Education(
+      'Apr 2016',
+      'Apr 2018',
+      'TEO International',
+      'Sr. Software Engineer',
+    ),
+    Education(
+      'July 2014',
+      'March 2016',
+      'Citrusbits',
+      'Software Engineer',
+    ),
+  ];
+
+  Widget _buildEducation() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _buildEducationContainerHeading(),
+        _buildEducationSummary(),
+        const SizedBox(height: 8.0),
+        _buildEducationTimeline(),
+      ],
+    );
+  }
+
+  Widget _buildEducationContainerHeading() {
+    return Text(
+      Strings.experience,
+      style: TextStyles.subHeading,
+    );
+  }
+
+  Widget _buildEducationSummary() {
+    return Text(
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      style: TextStyles.body,
+    );
+  }
+
+  Widget _buildEducationTimeline() {
+    final List<Widget> widgets = educationList
+        .map((education) => _buildEducationTile(education))
+        .toList();
+    return Column(children: widgets);
+  }
+
+  Widget _buildEducationTile(Education education) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            education.post,
+            style: TextStyles.company,
+          ),
+          Text(
+            education.organization,
+            style: TextStyles.body.copyWith(
+              color: const Color(0xFF45405B),
+            ),
+          ),
+          Text(
+            '${education.from}-${education.to}',
+            style: TextStyles.body,
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Footer Methods:------------------------------------------------------------
+  Widget _buildFooter(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Align(
+                child: _buildCopyRightText(context),
+                alignment: Alignment.centerLeft,
+              ),
+              Align(
+                child: _buildSocialIcons(),
+                alignment: Alignment.centerRight,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCopyRightText(BuildContext context) {
+    return Text(
+      Strings.rightsReserved,
+      style: TextStyles.body1.copyWith(
+        fontSize: ResponsiveWidget.isSmallScreen(context) ? 8 : 10.0,
+      ),
+    );
+  }
+
+  Widget _buildSocialIcons() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            window.open("https://www.linkedin.com/in/zubairehman/", "LinkedIn");
+          },
+          child: Image.network(
+            Assets.linkedin,
+            color: const Color(0xFF45405B),
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+        const SizedBox(width: 16.0),
+        GestureDetector(
+          onTap: () {
+            window.open("https://medium.com/@zubairehman.work", "Medium");
+          },
+          child: Image.network(
+            Assets.evernote,
+            color: const Color(0xFF45405B),
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+        const SizedBox(width: 16.0),
+        GestureDetector(
+          onTap: () {
+            window.open("https://github.com/zubairehman", "Github");
+          },
+          child: Image.network(
+            Assets.google,
+            color: const Color(0xFF45405B),
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+        const SizedBox(width: 16.0),
+        GestureDetector(
+          onTap: () {
+            window.open("https://twitter.com/zubair340", "Twitter");
+          },
+          child: Image.network(
+            Assets.twitter,
+            color: const Color(0xFF45405B),
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
